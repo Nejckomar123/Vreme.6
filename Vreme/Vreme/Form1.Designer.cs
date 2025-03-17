c8100e2919367f88358e060e31110226cdd5c609
Janez.Č <zeleznjakleon2005@gmail.com>
2025-03-17 17:33:52 +0100

    Add files via upload

---
+++ Vreme/Vreme/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Vreme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelHumidity = new System.Windows.Forms.Label();
+            this.labelForecast = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelCity
+            // 
+            this.labelCity.AccessibleName = "labelLat";
+            this.labelCity.AutoSize = true;
+            this.labelCity.Location = new System.Drawing.Point(298, 185);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(30, 13);
+            this.labelCity.TabIndex = 1;
+            this.labelCity.Tag = "labelLat";
+            this.labelCity.Text = "City: ";
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AccessibleName = "labelLon";
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Location = new System.Drawing.Point(298, 198);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(37, 13);
+            this.labelTemperature.TabIndex = 2;
+            this.labelTemperature.Text = "Temp:";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(298, 211);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(63, 13);
+            this.labelDescription.TabIndex = 3;
+            this.labelDescription.Text = "Description:";
+            // 
+            // labelHumidity
+            // 
+            this.labelHumidity.AutoSize = true;
+            this.labelHumidity.Location = new System.Drawing.Point(298, 224);
+            this.labelHumidity.Name = "labelHumidity";
+            this.labelHumidity.Size = new System.Drawing.Size(50, 13);
+            this.labelHumidity.TabIndex = 4;
+            this.labelHumidity.Text = "Humidity:";
+            this.labelHumidity.Click += new System.EventHandler(this.labelHumidity_Click);
+            // 
+            // labelForecast
+            // 
+            this.labelForecast.AutoSize = true;
+            this.labelForecast.Location = new System.Drawing.Point(298, 237);
+            this.labelForecast.Name = "labelForecast";
+            this.labelForecast.Size = new System.Drawing.Size(51, 13);
+            this.labelForecast.TabIndex = 5;
+            this.labelForecast.Text = "Forecast:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter City:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(508, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Press \'Enter\'";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelForecast);
+            this.Controls.Add(this.labelHumidity);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelTemperature);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Weather";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelHumidity;
+        private System.Windows.Forms.Label labelForecast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Vreme/Vreme/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Vreme
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string ApiKey = "2ea8e51332377ce7ae6d78634925dd75"; // Single API key
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyDown += textBox1_KeyDown;
+        }
+
+        private async void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                string city = textBox1.Text;
+                await GetAndDisplayGeoLocation(city);
+            }
+        }
+
+        private async Task GetAndDisplayGeoLocation(string city)
+        {
+            List<WeatherApi.GeoLocation> locations = await WeatherApi.GetGeoLocation(city);
+
+            if (locations != null && locations.Count > 0)
+            {
+                WeatherApi.GeoLocation location = locations[0];
+                labelCity.Text = $"City: {location.name}";
+
+                await GetAndDisplayWeather(location.lat, location.lon);
+                await GetAndDisplayForecast(location.lat, location.lon);
+            }
+            else
+            {
+                MessageBox.Show("Failed to retrieve geolocation data.");
+            }
+        }
+
+        private async Task GetAndDisplayWeather(double lat, double lon)
+        {
+            WeatherApi.WeatherData weatherData = await WeatherApi.GetWeatherData(lat, lon);
+
+            if (weatherData != null)
+            {
+                labelTemperature.Text = $"Temperature: {weatherData.main.temp - 273.15:F2} °C";
+                labelDescription.Text = $"Description: {weatherData.weather[0].description}";
+                labelHumidity.Text = $"Humidity: {weatherData.main.humidity}%";
+            }
+            else
+            {
+                MessageBox.Show("Failed to retrieve weather data.");
+            }
+        }
+
+        private async Task GetAndDisplayForecast(double lat, double lon)
+        {
+            WeatherApi.ForecastData forecastData = await WeatherApi.GetForecastData(lat, lon);
+
+            if (forecastData != null && forecastData.list.Count > 0)
+            {
+                string forecastText = "3-Day Forecast:\n";
+
+                for (int i = 0; i < 8 * 3; i += 8)
+                {
+                    DateTime forecastTime = DateTimeOffset.FromUnixTimeSeconds(forecastData.list[i].dt).DateTime.ToLocalTime();
+                    double temp = forecastData.list[i].main.temp - 273.15;
+                    string description = forecastData.list[i].weather[0].description;
+                    forecastText += $"{forecastTime.ToShortDateString()}: {temp:F2} °C, {description}\n";
+                }
+                labelForecast.Text = forecastText;
+            }
+            else
+            {
+                MessageBox.Show("Failed to retrieve forecast data.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelHumidity_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public partial class WeatherApi
+    {
+        private static readonly string ApiKey = "2ea8e51332377ce7ae6d78634925dd75"; // Single API key
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<List<GeoLocation>> GetGeoLocation(string city)
+        {
+            try
+            {
+                string url = $"http://api.openweathermap.org/geo/1.0/direct?q={Uri.EscapeDataString(city)}&appid={ApiKey}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+                List<GeoLocation> locations = JsonConvert.DeserializeObject<List<GeoLocation>>(json);
+
+                return locations;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting geolocation: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<WeatherData> GetWeatherData(double lat, double lon)
+        {
+            try
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+                WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(json);
+
+                return weatherData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting weather data: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<ForecastData> GetForecastData(double lat, double lon)
+        {
+            try
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={ApiKey}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+                ForecastData forecastData = JsonConvert.DeserializeObject<ForecastData>(json);
+
+                return forecastData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting forecast data: {ex.Message}");
+                return null;
+            }
+        }
+
+        public class GeoLocation
+        {
+            public string name { get; set; }
+            public double lat { get; set; }
+            public double lon { get; set; }
+            public string country { get; set; }
+            public string state { get; set; }
+        }
+
+        public class WeatherData
+        {
+            public MainData main { get; set; }
+            public List<WeatherDescription> weather { get; set; }
+        }
+
+        public class MainData
+        {
+            public double temp { get; set; }
+            public int humidity { get; set; }
+        }
+
+        public class WeatherDescription
+        {
+            public string description { get; set; }
+        }
+
+        public class ForecastData
+        {
+            public List<ForecastItem> list { get; set; }
+        }
+
+        public class ForecastItem
+        {
+            public long dt { get; set; }
+            public MainData main { get; set; }
+            public List<WeatherDescription> weather { get; set; }
+        }
+    }
+}
